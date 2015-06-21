a61255f33e066b3e89cf80df288f52021b4294db
wilisort <wilfrido.ortega@gmail.com>
2015-06-21 19:43:23 -0400

    Update cachingW3.R

--- cachingW3.R
+++ cachingW3.R
@@ -20,6 +20,24 @@ makeCacheMatrix <- function(x = matrix()) {
 	     get_inverse = get_inverse)
 }
 
+## CacheSolve calculates the inverse of the special "matrix" created with makeCacheMatrix function.
+## It checks if the inverse has been calculated. 
+## If yes it gets the inverse from the cache and skips computation. 
+## Otherwise, it calculates the inverse matrix and sets the inverse in the cache  
+## using 'set_inverse' function.
+
+cacheSolve <- function(x, ...) {				## Setting inverse matrix
+	inverse <- x$get_inverse()				## Getting inverse
+	if(!is.null(inverse)) {					## Checking if inverse already computed
+		message("getting cached data")			## Displaying cached message
+		return(inverse)
+	}
+	data <- x$get_matrix()					## Getting Matrix
+	inverse <- solve(data, ...)				## Computing inverse by using solve() 
+	x$set_inverse(inverse)					## To cache the inverse
+	inverse 						## Returning the inverse
+}
+
 m <- matrix(c(-1, -2, 1, 1,5,7,0,9,-10), 3,3)
 # m input matrix 3*3 that is invertible
 m  #displays input matrix
